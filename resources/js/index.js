0b386aac36ec08087abb4f756115d849b1f55bf7
Thi Ha My Pham <hamypham2000@gmail.com>
2022-05-17 22:18:04 +0200

    Start game + Countdown function

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,6 +11,45 @@ function onWordlistAvailable(wordlist) {
     // Prints the first 10 entries from the received word list
     console.log(wordlist.slice(0, 10));
     // TODO: Start your implementation here
+
 }
 
 init();
+
+// Countdown-Function
+let defaultTime = 5;
+const wordInput = document.querySelector(".word-input");
+let firstHit = true;
+
+function startGame() { 
+    wordInput.addEventListener("keydown", function(event)
+    {
+    if (firstHit) {
+        console.log("Game started");
+        countDown();
+        firstHit = false;
+    }
+    });
+}
+
+function countDown() {
+    setInterval(function() {
+        if (defaultTime >= 0) {
+            if (defaultTime < 10) {
+                document.getElementsByClassName('timer-output')[0].innerHTML = "00:0" + defaultTime;
+            } else {
+                document.getElementsByClassName('timer-output')[0].innerHTML = "00:" + defaultTime;
+            }
+            defaultTime--;
+        } else {
+            clearInterval();
+            document.getElementsByClassName('timer-output')[0].innerHTML = "";
+            document.getElementsByClassName('word-input')[0].disabled = true;
+        }
+    },1000)
+}
+
+startGame();
+
+
+
