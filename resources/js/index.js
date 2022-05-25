156a5fb19d60c1b5c60cbe1e6d877376a1910856
Thi Ha My Pham <hamypham2000@gmail.com>
2022-05-25 23:15:35 +0200

    Improve code quality with code reviews

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,43 +1,51 @@
 /* eslint-env browser */
-//#############################################################
-//                         import                             #
-//#############################################################
+
 import WordList from "./wordlist/WordList.js";
 
-//#############################################################
-//                         variables                          #
-//#############################################################
-var numOfWords = 0;
-var correctAnswers = 0;
-let defaultTime = 59;
+let numOfWords = 0,
+    correctAnswers = 0,
+    defaultTime = 59;
+var firstHit = true;
 const wordInput = document.querySelector(".word-input");
-let firstHit = true;
 
-//#############################################################
-//                            private                         #
-//#############################################################
+function init() {
+/* This function reads and the
+   romeo-and-juliet-word-list.json and loads it as class object Wordlist. */
+
+    WordList.loadList().then(onWordlistAvailable);
+    startGame();
+}
+
+function startGame() { 
+/* Starts game with enter input and
+   calls function countdown. */
+    wordInput.addEventListener("keydown", function(event)
+    {
+    if (firstHit) {
+        countDown();
+        firstHit = false;
+    }
+    });
+}
+
+init();
+
 
 function wordCompare(jsonData) {
     /* Compares the entries from the user input data,
-     result-list, with the data of jsonData, an ordered list
-      of pairs of words with counts; if a word from the jsonData
-       is entered, the event shows the word an its count in the
-        corpus.*/
+       result-list, with the data of jsonData, an ordered list
+       of pairs of words with counts; if a word from the jsonData
+       is entered, the event shows the word an its count in the corpus. */
     let resultList = Array.from(document.body.querySelectorAll(".result-list"));
     wordInput.addEventListener("keypress", function (event) {
-        if (event.key === "Enter" && wordInput.value != "") {
+        if (event.key === "Enter" && wordInput.value !== "") {
             for (let i = 0; i < jsonData.length; i++) {
-                if (jsonData[i].word === wordInput.value) {
-                    if (resultList.includes(wordInput.value)) {
-                        console.log("duplicate");
-                    } else {
-                        resultList.unshift(wordInput.value);
-                        document.getElementsByClassName("result-list")[0].innerHTML += 
-                            '<li><span class="count">' + jsonData[i].count + '</span>'
-                                + '<span class="word">' + jsonData[i].word + '</span></li>';
-                        correctAnswers++;
-                        console.log("entered");
-                    }
+                if (jsonData[i].word === wordInput.value && !resultList.includes(wordInput.value)) {
+                    resultList.unshift(wordInput.value);
+                    document.getElementsByClassName("result-list")[0].innerHTML += 
+                        '<li><span class="count">' + jsonData[i].count + '</span>'
+                            + '<span class="word">' + jsonData[i].word + '</span></li>';
+                    correctAnswers++;
                 }
             }
             wordInput.value = "";
@@ -46,21 +54,17 @@ function wordCompare(jsonData) {
 
 function onWordlistAvailable(wordlist) {
     /* Logs the first 10 entries from the received word list
-     and throws an error if import doesn't work.*/
-    try {
-    console.log("### WordList received ###");
-    console.log(wordlist.slice(0, 10));
+       and throws an error if import doesn't work. */
+    
     numOfWords += wordlist.length;
-    wordCompare(wordlist);  }
-    catch(error) {
-        alert("Function onWordlistAvailable: Import error !");
-      }
+    wordCompare(wordlist);  
+    
 }
 
 function countDown() {
     /* Starts the countdown for 60 seconds for the user input
-     and ends it with a statement.*/
-    try {
+       and ends it with a statement. */
+    
     const interval = setInterval(function() {
         if (defaultTime >= 0) {
             if (defaultTime < 10) {
@@ -75,15 +79,11 @@ function countDown() {
             document.getElementsByClassName('word-input')[0].disabled = true;
             clearInterval(interval);
             scoreCounter();
-            console.log("Game over");
         }
-    },1000)
-}catch(error) {
-    alert("Function countdown: time event error!");
-  }
+    },1000);
+
 }
 
-// Score-Function
 function scoreCounter() {
     let coveredPercent = (correctAnswers/numOfWords) * 100;
     coveredPercent = coveredPercent.toFixed(2);
@@ -92,42 +92,3 @@ function scoreCounter() {
     + " words from Romeo and Juliet. These are ~" + coveredPercent
     + " percent of all unique words Shakespeare used to write the play!";
 }
-
-//#############################################################
-//                            public                          #
-//#############################################################
-
-function init() {/*This function reads and the
- romeo-and-juliet-word-list.json and loads it as class object
-  Wordlist.*/
-try {
-    WordList.loadList().then(onWordlistAvailable);
-}
-catch(error) {
-    alert("Function init: import error!");
-}
-}
-
-function startGame() { /* Starts game with enter input and
- calls function countdown.*/
-    try {
-    wordInput.addEventListener("keydown", function(event)
-    {
-    if (firstHit) {
-        console.log("Game started");
-        countDown();
-        firstHit = false;
-    }
-    });
-    }
-    catch(error) {
-        alert("Function startGame: event error !");
-      }
-}
-
-//#############################################################
-//                            run                             #
-//#############################################################
-
-init();
-startGame();
